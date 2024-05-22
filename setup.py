1c4bdb4f33e61bffbb083f8fae6ab8a69e2f84f4
AntonioVitoMastromarino <antonio.v.mastromarino@gmail.com>
2024-05-22 18:56:21 +0100

    so bat at names

--- setup.py
+++ setup.py
@@ -1,34 +1,5 @@
 from setuptools import setup, find_packages
 
-def get_readme():
-
-    with open('README.md') as f:
-        return f.read()
-
-setup(
-
-  name='naiveb',
-  description='Implementation of Naive Bayes Classifier methods.',
-  long_description=get_readme(),
-  version='0.0.0',
-  license='MIT License',
-  author='Antonio Mastromarino',
-  author_email='antonio.mastromarino@wolfson.ox.ac.uk',
-  maintainer='Antonio Mastromarino',
-  maintainer_email='antonio.mastromarino@wolfson.ox.ac.uk',
-  url='https://github.com/AntonioVitoMastromarino/NaiveB',
-
-  packages=find_packages(include=('naiveb', 'naiveb.*')),
-    install_requires=[
-      'numpy',
-      'matplotlib',
-      'pandas',
-      'scipy',
-    ],
-    extras_require={
-      'docs': [
-        'sphinx',
-        'sphinx_rtd_theme',
-      ],
-    },
-)
+setup(packages=find_packages(include=('empirbayes', 'empirbayes.*')),
+      install_requires=['numpy', 'matplotlib', 'pandas', 'scipy'],
+      name='empirbayes')
